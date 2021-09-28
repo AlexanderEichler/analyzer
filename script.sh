ecd9d01b6c3f773a3b87b42e1d84d839dcabd52a
AlexanderEichler <alexander.eichler197@gmail.com>
2021-09-28 09:47:54 +0200

    modified script

--- script.sh
+++ script.sh
@@ -2,10 +2,14 @@
 clear
 echo "Generating output of test runs of tests/regression/03-practical"
 dstpath=/home/alex/Documents/git/analyzer/testResults/
-for entry in "/home/alex/Documents/git/analyzer/tests/regression/03-practical"/*; do
-  echo "$entry"
-  basename=$(basename "$entry")
-  dst="${dstpath%.c}"$basename".sarif"
-  ./goblint --sarif -o "$dst" "$entry" 
+for folder in "/home/alex/Documents/git/analyzer/tests/regression"/*; do
+	foldername=$(basename "$folder")
+	echo "$foldername"
+	mkdir "/home/alex/Documents/git/analyzer/testResults"/$foldername
+	for entry in "/home/alex/Documents/git/analyzer/tests/regression/03-practical"/*; do
+	  
+	  basename=$(basename "$entry")
+	  dst="${dstpath%.c}"$foldername"/"$basename".sarif"
+	  ./goblint --sarif -o "$dst" "$entry" 
+	done
 done
-
